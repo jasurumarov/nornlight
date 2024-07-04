4637b4fa96ab4d724e2afef44591fd6e85563950
jasurumarov <jasurumarov199@gmail.com>
2024-07-04 18:10:44 +0500

    finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useGetProductsQuery } from '../../context/api/productsApi'
 
 // Icons
 import Logo from '../../assets/icons/logo.svg'
@@ -12,12 +13,20 @@ import { FiSearch } from 'react-icons/fi'
 
 
 const Header = () => {
-    const navigate = useNavigate()
-
+    const [menu, setMenu] = useState(false)
     const [shrink, setShrink] = useState(false)
-    
+    const [searchValue, setSearchValue] = useState('')
+    const [filteredData, setFilteredData] = useState(null)
+
+    const navigate = useNavigate()
     let favorites = useSelector(state => state.wishlist.value)
     let cart = useSelector(state => state.cart.value)
+    const { data } = useGetProductsQuery()
+    useEffect(() => {
+        if (data && searchValue) {
+            setFilteredData(data.filter(product => product.title.toLowerCase().includes(searchValue.trim().toLowerCase())))
+        }
+    }, [searchValue])
 
     useEffect(() => {
         const handleScroll = () => {
@@ -39,19 +48,44 @@ const Header = () => {
             <div className="container">
                 <nav>
                     <div className="header__logo-side">
-                        <button>
+                        <button onClick={() => setMenu(!menu)} >
                             <span></span>
                             <span></span>
                             <span></span>
                         </button>
+                        <ul className={menu ? 'show' : ''}>
+                            <NavLink className='/about-us'>О компании</NavLink>
+                            <NavLink to={'/shipping'}>Доставка и оплата</NavLink>
+                            <NavLink to={'/return'}>Возврат</NavLink>
+                            <NavLink to={'/garant'}>Гарантии</NavLink>
+                            <NavLink to={'/contact'}>Контакты</NavLink>
+                            <NavLink to={'/blog'}>Блог</NavLink>
+                            <div>
+                                <button onClick={() => navigate('/catalog')}><RiMenu2Line /> Каталог</button>
+                                <Link to={'tel: 8 (800) 890-46-56'}>8 (800) 890-46-56</Link>
+                                <p>Заказать звонок</p>
+                            </div>
+                        </ul>
                         <Link to={'/'}>
                             <img src={Logo} alt="site logo" />
                         </Link>
                     </div>
-                    <button className='header__category-btn'><RiMenu2Line /> Каталог</button>
+                    <button onClick={() => navigate('/catalog')} className='header__category-btn'><RiMenu2Line /> Каталог</button>
                     <label>
-                        <input type="search" placeholder='Поиск по товарам' />
+                        <input className={`${searchValue.trim() ? 'searchedInput' : ''} ${filteredData?.length ? '' : 'noSearchedInput'}`} value={searchValue} onChange={(e) => setSearchValue(e.target.value)} type="search" placeholder='Поиск по товарам' />
                         <FiSearch />
+                        {
+                            searchValue.trim() ?
+                                <ul className='search__value'>
+                                    {
+                                        filteredData?.slice(0, 5)?.map(el => (
+                                            <li onClick={() => navigate(`/products/${el.id}`)} key={el.id}><img src={el.url[0]} alt={el.title} /> <p>{el.title}</p></li>
+                                        ))
+                                    }
+                                </ul>
+                                :
+                                <></>
+                        }
                     </label>
                     <div className='header__btns'>
                         <button onClick={() => navigate('/favorites')}>
